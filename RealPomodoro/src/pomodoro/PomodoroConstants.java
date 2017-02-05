5bfe825e54104e348dccc58eee9281b10f016c69
Marlon Mendes <marlonacademy@hotmail.com>
2017-02-05 11:42:45 -0200

    Setting correct constants for pomodoros

--- RealPomodoro/src/pomodoro/PomodoroConstants.java
+++ RealPomodoro/src/pomodoro/PomodoroConstants.java
@@ -1,7 +1,7 @@
 package pomodoro;
 
 public class PomodoroConstants {
-	public static final Integer DEFAULT_MINUTES = 1;
-	public static final Integer DEFAULT_SECONDS = 2;
-	public static final Integer SECONDS_IN_A_MINUTE = 2;
+	public static final Integer DEFAULT_MINUTES = 25;
+	public static final Integer DEFAULT_SECONDS = 00;
+	public static final Integer SECONDS_IN_A_MINUTE = 60;
 }
